22c6760b39a03896e5ae9154b7e19429418fa004
colakalpeer <colakalpeer93@gmail.com>
2024-03-29 18:57:43 +0300

    ui: improvements made

--- project-2.js
+++ project-2.js
@@ -6,16 +6,16 @@ let closeBtn = document.getElementById('close-btn');
 
 // Event Listeners
 
-openBtn.addEventListener('click', function() {
-    modalContainer.style.display = 'block';
+openBtn.addEventListener('click', function (){
+  modalContainer.style.display = 'block';
 });
 
-closeBtn.addEventListener('click', function() {
-    modalContainer.style.display = 'none';
-})
+closeBtn.addEventListener('click', function (){
+  modalContainer.style.display = 'none';
+});
 
-window.addEventListener('click', function(e) {
-    if (e.target === modalContainer) {
-        modalContainer.style.display = 'none';
-    }
+window.addEventListener('click', function (e){
+  if (e.target === modalContainer) {
+    modalContainer.style.display = 'none';
+  }
 });
